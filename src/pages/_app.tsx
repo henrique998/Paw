bb2e44e011fc8bdebcfc2f1f32fdb0291450ff1a
Henrique Monteiro <henriquemonteiro037@gmail.com>
2022-09-27 16:44:18 +0000

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/global'
+import { defaultTheme } from '../styles/themes/default'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus {
+        outline: 0;
+    }
+
+    body {
+        background-color: ${(props) => props.theme['base-background']};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        /* font-family: 'Inter', sans-serif; */
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    ul {
+        list-style: none;
+    }
+    
+    button {
+        cursor: pointer;
+        background: transparent;
+        border: 0;
+    }
+`
